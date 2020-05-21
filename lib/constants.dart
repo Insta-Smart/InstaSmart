498ab5507a380dd2d2faeb38242203bc9c0dd77a
Mathew <noelmathewisaac@gmail.com>
2020-05-21 20:19:13 +0800

    Updated firebase account

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+
+class Constants {
+  static const brightPurple = Color(0xff8276D8);
+  static const darkPurple = Color(0xff635AA1);
+  static const paleBlue = Color(0xffA3C5DE);
+  static const palePink = Color(0xffDF6666);
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:instasmart/home_screen.dart';
+import 'login_screen.dart';
+import 'home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'InstaSmart',
+      theme: ThemeData(
+//      brightness: Brightness.dark,
+        primarySwatch: Colors.indigo,
+        backgroundColor: Colors.orange,
+        accentColor: Colors.teal[300],
+        cursorColor: Colors.deepPurple,
+        // fontFamily: 'SourceSansPro',
+        textTheme: TextTheme(
+          headline3: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 45.0,
+            // fontWeight: FontWeight.w400,
+            color: Colors.white,
+          ),
+          button: TextStyle(
+            // OpenSans is similar to NotoSans but the uppercases look a bit better IMO
+            fontFamily: 'OpenSans',
+          ),
+          caption: TextStyle(
+            fontFamily: 'NotoSans',
+            fontSize: 12.0,
+            fontWeight: FontWeight.normal,
+            color: Colors.deepPurple[300],
+          ),
+          headline1: TextStyle(fontFamily: 'Quicksand'),
+          headline2: TextStyle(fontFamily: 'Quicksand'),
+          headline4: TextStyle(fontFamily: 'Quicksand'),
+          headline5: TextStyle(fontFamily: 'NotoSans'),
+          headline6: TextStyle(fontFamily: 'NotoSans'),
+          subtitle1: TextStyle(fontFamily: 'NotoSans'),
+          bodyText1: TextStyle(fontFamily: 'NotoSans'),
+          bodyText2: TextStyle(fontFamily: 'NotoSans'),
+          subtitle2: TextStyle(fontFamily: 'NotoSans'),
+          overline: TextStyle(fontFamily: 'NotoSans'),
+        ),
+      ),
+      home: LoginScreen(),
+      routes: {
+        LoginScreen.routeName: (context) => LoginScreen(),
+        HomeScreen.routeName: (context) => HomeScreen(),
+
+      },
+    );
+  }
+}
