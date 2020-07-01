14696f2cb5b66e4b7ed02c17525558e63141399d
vanshiqa <e0411071@u.nus.edu>
2020-07-01 14:06:41 +0800

    Frames screen - changed gridview.count to gridview.builder and removed futurebuilder

--- lib/models/login_functions.dart
+++ lib/models/login_functions.dart
@@ -3,10 +3,12 @@ import 'package:flutter_login/flutter_login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:instasmart/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class FirebaseFunctions extends ChangeNotifier {
   final auth = FirebaseAuth.instance;
   final db = Firestore.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
   User currUser;
 
   User _userFromFirebase(FirebaseUser user) {
@@ -92,5 +94,52 @@ class FirebaseFunctions extends ChangeNotifier {
         r'^(?=.{8,})(?=.*[A-Z])(?=.*[0-9])'; //Minimum 8 characters with atleast 1 Uppercase and 1 Numeric character
     RegExp regex = new RegExp(pattern);
     return (!regex.hasMatch(value)) ? false : true;
+  }
+
+  GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: <String>[
+      'email',
+    ],
+  );
+
+  Future<String> signInWithGoogle() async {
+    try {
+//      try {
+//        await _googleSignIn.signIn();
+//      } catch (error) {
+//        print(error);
+//      }
+//    }
+
+      final GoogleSignInAccount googleSignInAccount =
+          await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      final AuthResult authResult = await auth.signInWithCredential(credential);
+      final FirebaseUser user = authResult.user;
+
+      assert(!user.isAnonymous);
+      assert(await user.getIdToken() != null);
+
+      final FirebaseUser currentUser = await auth.currentUser();
+      assert(user.uid == currentUser.uid);
+      currUser = _userFromFirebase(currentUser);
+
+      return 'signInWithGoogle succeeded: $user';
+    } catch (e) {
+      print("Google sign in error: ${e}");
+    }
+  }
+
+  void signOutGoogle() async {
+    await googleSignIn.signOut();
+
+    print("User Sign Out");
   }
 }
