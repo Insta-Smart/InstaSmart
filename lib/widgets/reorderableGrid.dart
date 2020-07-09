f2dd6aa334633a7069ddca6e0010faf38a93ae5d
vanshiqa <e0411071@u.nus.edu>
2020-07-09 16:45:00 +0800

    account screen and notifications

--- lib/widgets/reorderableGrid.dart
+++ lib/widgets/reorderableGrid.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:instasmart/models/size_config.dart';
 import 'package:instasmart/models/user.dart';
+import 'package:instasmart/screens/create_grid_screen.dart';
 import 'package:instasmart/screens/final_grid_screen.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:reorderables/reorderables.dart';
@@ -118,22 +119,17 @@ class ReorderableGrid extends StatelessWidget {
                                                               snapshot
                                                                   .data[index]);
                                                       saveImages([imgBytes]);
-                                                      AwesomeDialog(
-                                                        context: context,
-                                                        headerAnimationLoop:
-                                                            false,
-                                                        dialogType:
-                                                            DialogType.SUCCES,
-                                                        animType: AnimType
-                                                            .BOTTOMSLIDE,
-                                                        title: 'Saved',
-                                                        desc:
-                                                            'Image have been saved to gallery',
-                                                        btnOkOnPress: () {
-                                                          Navigator.pop(
-                                                              context);
-                                                        },
-                                                      )..show();
+                                                      showDialog(
+                                                          context: context,
+                                                          builder: (BuildContext
+                                                                  context) =>
+                                                              CustomDialogWidget(
+                                                                title: 'Saved!',
+                                                                body:
+                                                                    'Images have been saved to gallery',
+                                                              ));
+//
+//
                                                     },
                                                   ),
                                                   ListTile(
